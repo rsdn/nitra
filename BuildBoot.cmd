b99e3a7afaa3344ca9d4d87e282e64e2e804155a
VladD2 <vc@rsdn.ru>
2017-10-05 21:34:21 +0300

    Use the MSBuild from VS2017.

--- BuildBoot.cmd
+++ BuildBoot.cmd
@@ -1,5 +1,7 @@
 @echo off
 title BuildBoot
 SET MSBUILDENABLEALLPROPERTYFUNCTIONS=1
-%WinDir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe %~dp0\Common\BootTasks.proj /t:BuildBoot /tv:4.0
+rem %WinDir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe %~dp0\Common\BootTasks.proj /t:BuildBoot /tv:4.0
+call "%VSINSTALLDIR%\Common7\Tools\VsDevCmd.bat"
+MSBuild.exe %~dp0\Common\BootTasks.proj /t:BuildBoot /tv:4.0 /bl:%~dp0\Boot2\Net-4.0\BootTasks.binlog;ProjectImports=Embed
 pause
