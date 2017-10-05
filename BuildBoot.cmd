1216238ac037453a5e9accbd95e81be285876cbe
VladD2 <vc@rsdn.ru>
2017-10-05 21:35:05 +0300

    Fix BuildBoot.cmd

--- BuildBoot.cmd
+++ BuildBoot.cmd
@@ -3,5 +3,5 @@ title BuildBoot
 SET MSBUILDENABLEALLPROPERTYFUNCTIONS=1
 rem %WinDir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe %~dp0\Common\BootTasks.proj /t:BuildBoot /tv:4.0
 call "%VSINSTALLDIR%\Common7\Tools\VsDevCmd.bat"
-MSBuild.exe %~dp0\Common\BootTasks.proj /t:BuildBoot /tv:4.0 /bl:%~dp0\Boot2\Net-4.0\BootTasks.binlog;ProjectImports=Embed
+MSBuild.exe %~dp0\Common\BootTasks.proj /t:BuildBoot /tv:4.0 /bl:%~dp0\Boot2\BootTasks.binlog;ProjectImports=Embed
 pause
