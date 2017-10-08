3bb393bfb3b1e6c1c5629e13c65f0fb9e79f6af1
VladD2 <vc@rsdn.ru>
2017-10-08 22:05:40 +0300

    Work on the DependentProperties debugging staff.

--- RebuildBoot.cmd
+++ RebuildBoot.cmd
@@ -1,5 +1,8 @@
 @echo off
 title RebuildBoot
 SET MSBUILDENABLEALLPROPERTYFUNCTIONS=1
-%WinDir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe %~dp0\Common\BootTasks.proj /t:BuildBoot /tv:4.0 /p:BuildTarget=Rebuild
+rem %WinDir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe %~dp0\Common\BootTasks.proj /t:BuildBoot /tv:4.0
+call "%VSINSTALLDIR%\Common7\Tools\VsDevCmd.bat"
+MSBuild.exe %~dp0\Common\BootTasks.proj /t:BuildBoot /tv:4.0 /bl:%~dp0\Boot2\BootTasks.binlog;ProjectImports=Embed /p:BuildTarget=Rebuild
 pause
+
