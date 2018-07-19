6580d88dd735b52f7577c952fc32abe490380721
VladD2 <vc@rsdn.ru>
2018-07-20 01:12:32 +0300

    1. Remove dependency on System.Collections.Immutable.
    2. Add the light version of System.Collections.Immutable (System.Collections.Immutable.Light project). This project build with .Net Framework 4.5.
    3. Update NuGet packages on new versions.
    4. Move VS plugins related projects to .Net Framework 4.6.1.

--- BuildBoot.cmd
+++ BuildBoot.cmd
@@ -1,7 +1,19 @@
 @echo off
 title BuildBoot
+IF "%VS150COMNTOOLS%"=="" (
+    echo The VS150COMNTOOLS environment variable not set! Run this butch file from "Developer Command Prompt for VS 2017".
+    pause
+    EXIT /B 1
+)
+SET VsDevCmdPath=%VS150COMNTOOLS%VsDevCmd.bat
+IF NOT EXIST "%VsDevCmdPath%" (
+    echo The "%VsDevCmdPath%" butch file not exists! Try to run this butch file from "Developer Command Prompt for VS 2017".
+    pause
+    EXIT /B 1
+)
+
+call "%VsDevCmdPath%"
+rem See https://github.com/Microsoft/msbuild/blob/master/documentation/wiki/MSBuild-Tips-&-Tricks.md
 SET MSBUILDENABLEALLPROPERTYFUNCTIONS=1
-rem %WinDir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe %~dp0\Common\BootTasks.proj /t:BuildBoot /tv:4.0
-call "%VSINSTALLDIR%\Common7\Tools\VsDevCmd.bat"
 MSBuild.exe %~dp0\Common\BootTasks.proj /t:BuildBoot /tv:4.0 /bl:%~dp0\Boot2\BootTasks.binlog;ProjectImports=Embed
 pause
