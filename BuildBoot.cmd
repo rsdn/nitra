bfe0230d1b4505c348d39c9f4186a986219a26df
VladD2 <Vladislav.Chistyakov@kaspersky.com>
2018-10-02 16:55:41 +0300

    Fix BuildBoot.cmd

--- BuildBoot.cmd
+++ BuildBoot.cmd
@@ -1,19 +1,22 @@
 @echo off
 title BuildBoot
-IF "%VS150COMNTOOLS%"=="" (
-    echo The VS150COMNTOOLS environment variable not set! Run this butch file from "Developer Command Prompt for VS 2017".
-    pause
-    EXIT /B 1
-)
-SET VsDevCmdPath=%VS150COMNTOOLS%VsDevCmd.bat
-IF NOT EXIST "%VsDevCmdPath%" (
-    echo The "%VsDevCmdPath%" butch file not exists! Try to run this butch file from "Developer Command Prompt for VS 2017".
-    pause
-    EXIT /B 1
+
+SET MSBUILDENABLEALLPROPERTYFUNCTIONS=1
+setlocal ENABLEEXTENSIONS
+set KEY_NAME="HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Microsoft\VisualStudio\SxS\VS7"
+set VALUE_NAME=15.0
+
+for /f "tokens=3" %%a in ('reg query %KEY_NAME% /V %VALUE_NAME%  ^|findstr /ri "REG_SZ"') DO set Value=%%a
+
+if defined Value (
+call "%Value%Common7\Tools\VsDevCmd.bat"
+set NoPause=true
+
+MSBuild.exe %~dp0\Common\BootTasks.proj /t:BuildBoot /tv:4.0 /bl:%~dp0\Boot2\BootTasks.binlog;ProjectImports=Embed
+
+IF %errorlevel% == 0 call Reg-bins-VS2015.cmd
+) else (
+@echo %KEY_NAME%\%VALUE_NAME% not found.
 )
 
-call "%VsDevCmdPath%"
-rem See https://github.com/Microsoft/msbuild/blob/master/documentation/wiki/MSBuild-Tips-&-Tricks.md
-SET MSBUILDENABLEALLPROPERTYFUNCTIONS=1
-MSBuild.exe %~dp0\Common\BootTasks.proj /t:BuildBoot /tv:4.0 /bl:%~dp0\Boot2\BootTasks.binlog;ProjectImports=Embed
 pause
